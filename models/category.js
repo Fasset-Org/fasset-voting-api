cd74a8444dc75866937e3e1a8f10dd5c0feb22a9
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-18 11:26:12 +0200

    Create category.js

---
+++ models/category.js
@@ -0,0 +1,48 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Category.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      category: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      level: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "Category",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: "dateUpdated"
+    }
+  );
+  return Category;
+};
