2bb3d2e20a731a5323172cd2aa0c4d7088b2c886
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-18 11:26:38 +0200

    Create employee.js

---
+++ models/employee.js
@@ -0,0 +1,53 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Employee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ UserVoteCategory }) {
+      // define association here
+
+      this.hasMany(UserVoteCategory, { foreignKey: "employeeId" });
+    }
+  }
+  Employee.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4
+      },
+      fullName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      position: {
+        type: DataTypes.TEXT
+      },
+      type: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "Employee",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: "dateUpdated"
+    }
+  );
+  return Employee;
+};
