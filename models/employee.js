f7db97ec46008053a15870c6efad0447b32006a1
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-18 21:36:26 +0200

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -7,10 +7,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ UserVoteCategory }) {
+    static associate({ Vote }) {
       // define association here
 
-      this.hasMany(UserVoteCategory, { foreignKey: "employeeId" });
+      this.hasMany(Vote, { foreignKey: "employeeId" });
     }
   }
   Employee.init(
